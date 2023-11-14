9353b8c7624a578ebc91193a71fe4e0210394cc6
Pontes <hpo.pontes@gmail.com>
2023-11-14 12:10:41 -0300

    Home box maior

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import './Home.scss'
+import { Link } from 'react-router-dom';
 const Home = () => {
   return (
-    <div><h2>Home editando essa bagaça</h2></div>
+    <section className='home-sec'>
+      <h2>Global Soluction 2023 - Engenharia de Software</h2>
+      <div className='card-imagem'>
+        <img src="../../public/bg.jpg" alt="imagem-home" />
+      </div>
+      <div className='box-baixo'>
+        <h2>Título da situação criada</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt fuga quo minima ducimus eveniet placeat explicabo obcaecati inventore assumenda eos?</p>
+      </div>
+      <div className='box-btn-info'>
+        <Link to ='/statistics'>Mais informações</Link>
+      </div>
+    </section>
   )
 }
 
