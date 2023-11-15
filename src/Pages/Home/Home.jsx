5cf7305e2c7351c52f5e4fd65bf5ab56d78da0b0
Pontes <hpo.pontes@gmail.com>
2023-11-15 07:06:32 -0300

    scss arrumado

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,38 +6,42 @@ const Home = () => {
   return (
     <>
     <section className='home-sec'>
-      <div className="home-div">
-        {/* <h2>Global Soluction 2023 - Engenharia de Software</h2> */}
-        <div className='card-imagem'>
-          <img src="../../public/bg.jpg" alt="imagem-home" />
+      <div className="max-home">
+        <div className="home-div">
+          {/* <h2>Global Soluction 2023 - Engenharia de Software</h2> */}
+          <div className='card-imagem'>
+            <img src="../../public/bg.jpg" alt="imagem-home" />
+          </div>
+          <div className='box-baixo'>
+            <h2>Título da situação criada</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt fuga quo minima ducimus eveniet placeat explicabo obcaecati inventore assumenda eos?</p>
+          </div>
+          <div className='box-btn-info'>
+            <Link to ='/users'>Mais informações</Link>
+          </div>
         </div>
-        <div className='box-baixo'>
-          <h2>Título da situação criada</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt fuga quo minima ducimus eveniet placeat explicabo obcaecati inventore assumenda eos?</p>
-        </div>
-        <div className='box-btn-info'>
-          <Link to ='/users'>Mais informações</Link>
+        <div className='box-direito'>
+          <div className="box-um">
+            <h3>Titulo do card</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, expedita.</p>
+          </div>
+          <hr />
+          <div className="box-dois">
+            <h3>Titulo do card</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, expedita.</p>
+          </div>
+          <hr />
+          <div className="box-tres">
+            <h3>Titulo do card</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, expedita.</p>
+          </div>
         </div>
       </div>
-      <div className='box-direito'>
-        <div className="box-um">
-          <h3>Titulo do card</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, expedita.</p>
-        </div>
-        <hr />
-        <div className="box-dois">
-          <h3>Titulo do card</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, expedita.</p>
-        </div>
-        <hr />
-        <div className="box-tres">
-          <h3>Titulo do card</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, expedita.</p>
-        </div>
+      <div className="max-vants">
+        <h1>Vantagens</h1>
+        <Vantagens/> 
       </div>
     </section>
-    <h1>Vantagens</h1>
-    <Vantagens/>
   </>
   )
 }
