658dae080a11f5ba4da5d7f5c02774bbc753c0a4
Pontes <hpo.pontes@gmail.com>
2023-11-15 12:28:46 -0300

    API 1

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -4,9 +4,27 @@ import { SHA256 } from 'crypto-js';
 import './Login.scss';
 
 function SignInSignupWithLocalStorage() {
+  const [dados, setDados] = useState(null);
+
+  useEffect(() => {
+    async function carregarDados() {
+      try {
+        const response = await fetch("http://localhost:3001/usuarios");
+        const dados = await response.json();
+        setDados(dados);
+        console.log('Código do usuário funcionou');
+      } catch (error) {
+        console.error("Erro ao carregar dados:", error);
+      }
+    }
+
+    carregarDados();
+  }, []);
+
   const name = useRef();
   const email = useRef();
   const password = useRef();
+  const especialidade = useRef(); // Adicionado para a especialidade
   const [showHome, setShowHome] = useState(false);
   const [show, setShow] = useState(false);
   const localSignUp = localStorage.getItem("signUp");
@@ -28,25 +46,84 @@ function SignInSignupWithLocalStorage() {
   };
 
   const handleClick = () => {
-    if (name.current.value && email.current.value && password.current.value) {
+    if (name.current.value && email.current.value && password.current.value && especialidade.current.value) {
+      // Verificar se já existe um médico com o mesmo nome e especialidade
+      const medicoExistente = dados && dados.usuarios && dados.usuarios.find(
+        (user) => user.nome === name.current.value && user.especialidade === especialidade.current.value
+      );
+  
+      if (medicoExistente) {
+        alert("Já existe um médico com o mesmo nome e especialidade");
+        return;
+      }
+  
       const hashedPassword = generateHash(password.current.value);
       localStorage.setItem("name", name.current.value);
       localStorage.setItem("email", email.current.value);
       localStorage.setItem("password", hashedPassword);
       localStorage.setItem("signUp", email.current.value);
+      // Adicionar especialidade ao localStorage
+      localStorage.setItem("especialidade", especialidade.current.value);
+  
       alert("Account created successfully!!");
       window.location.reload();
+    } else {
+      alert("Preencha todos os campos obrigatórios");
     }
   };
 
   const handleSignIn = () => {
     const hashedInputPassword = generateHash(password.current.value);
 
-    if (email.current.value === localEmail && hashedInputPassword === localPassword) {
+    if (
+      email.current.value === localEmail &&
+      hashedInputPassword === localPassword
+    ) {
       localStorage.setItem("signUp", email.current.value);
-      window.location.reload();
+
+      if (dados) {
+        const usuario = dados.usuarios.find(
+          (user) => user.email === localEmail
+        );
+
+        if (usuario) {
+          const papel = usuario.papel;
+
+          if (papel === "medico") {
+            // Carregar dados específicos para médicos
+            const medicoData = usuario;
+
+            // Aqui você pode fazer algo com os dados do médico
+            console.log("Dados do Médico:", medicoData);
+
+            alert("Login bem-sucedido como médico");
+          } else if (papel === "recepcionista") {
+            // Carregar dados específicos para recepcionistas
+            const recepcionistaData = {
+              nome: usuario.nome,
+              idade: usuario.idade,
+              exames: usuario.pacientes.map((paciente) => {
+                const pacienteData = dados.pacientes.find((p) => p.id === paciente);
+                return pacienteData.exames;
+              }).flat(),
+            };
+
+            // Aqui você pode fazer algo com os dados do recepcionista
+            console.log("Dados do Recepcionista:", recepcionistaData);
+
+            alert("Login bem-sucedido como recepcionista");
+          } else {
+            alert("Login bem-sucedido");
+          }
+          window.location.reload();
+        } else {
+          alert("Usuário não encontrado");
+        }
+      } else {
+        alert("Erro ao carregar dados");
+      }
     } else {
-      alert("Please Enter valid Credential");
+      alert("Credenciais inválidas");
     }
   };
 
@@ -69,15 +146,19 @@ function SignInSignupWithLocalStorage() {
         ) : (
           <div className="container">
             <h1>Login</h1>
-              <label htmlFor="Nome completo">Nome:</label>
+            <label htmlFor="Nome completo">Nome:</label>
             <div className="input_space">
               <input placeholder="Nome completo" type="text" ref={name} />
             </div>
-                <label htmlFor="E-mail">Email corporativo:</label>
+            <label htmlFor="E-mail">Email corporativo:</label>
             <div className="input_space">
               <input placeholder="E-mail" type="text" ref={email} />
             </div>
-                <label htmlFor="Password">Senha:</label>
+            <label htmlFor="Especialidade">Especialidade:</label>
+            <div className="input_space">
+              <input placeholder="Especialidade" type="text" ref={especialidade} />
+            </div>
+            <label htmlFor="Password">Senha:</label>
             <div className="input_space">
               <input placeholder="Password" type="password" ref={password} />
             </div>
--- src/Pages/Statics/index.jsx
+++ src/Pages/Statics/index.jsx
@@ -1,32 +1,115 @@
-import React from "react";
-import '../Login/Login.scss';
-// Esta pagina aparece após realizar o cadastro, seria a homepage do site com a conta do usuário
-function Homelog(){
-    const logout=()=>{
-        localStorage.removeItem("signUp")
-        window.location.reload()
+import React, { useState } from "react";
+import "../Login/Login.scss";
+
+function Homelog() {
+  const [searchInput, setSearchInput] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const logout = () => {
+    localStorage.removeItem("signUp");
+    window.location.reload();
+  };
+
+  const deleteAccount = () => {
+    localStorage.clear();
+    window.location.reload();
+  };
+
+  const procuraNoDadosJson = async () => {
+    try {
+      const responsePacientes = await fetch("http://localhost:3001/pacientes");
+      const responseExames = await fetch("http://localhost:3001/exames");
+
+      const dadosJsonPacientes = await responsePacientes.json();
+      const dadosJsonExames = await responseExames.json();
+
+      console.log("Resposta da API Pacientes:", dadosJsonPacientes);
+      console.log("Resposta da API Exames:", dadosJsonExames);
+
+      if (dadosJsonPacientes && dadosJsonPacientes.length > 0) {
+        const resultadosPesquisa = dadosJsonPacientes.filter((paciente) =>
+          paciente.nome.toLowerCase().includes(searchInput.toLowerCase())
+        );
+
+        console.log("Resultados da pesquisa Pacientes:", resultadosPesquisa);
+
+        if (resultadosPesquisa.length > 0) {
+          const resultadosComExames = resultadosPesquisa.map((paciente) => {
+            const examesDoPaciente = paciente.exames.map((exameId) =>
+              dadosJsonExames.find((exame) => exame.id === exameId)
+            );
+
+            return {
+              ...paciente,
+              exames: examesDoPaciente,
+            };
+          });
+
+          console.log("Resultados com Exames:", resultadosComExames);
+
+          setSearchResults(resultadosComExames);
+        } else {
+          console.log("Nenhum paciente encontrado com base na pesquisa.");
+        }
+      } else {
+        console.error("Dados inválidos ou não encontrados");
+      }
+    } catch (error) {
+      console.error("Erro ao buscar dados:", error);
     }
-    const deleteAccount=()=>{ 
-        localStorage.clear()
-        window.location.reload()
-    }
-    
-    return(
-        <section className="Homelog-sec">
-            <div className="credencial-log">
-                <div className="text-log">
-                    <p>Usuário: <span>{localStorage.getItem('name')}</span></p>
-                    <p>Cargo: <span>{localStorage.getItem('cargo')}</span></p>
-                </div>
-                <div className="btn-log">
-                    <button onClick={logout} className="logout">LogOut</button>
-                    <button onClick={deleteAccount} className="delete">Delete</button>
-                </div>
-            </div>
-            <div className="input_pesquisa">
-                <input type="text" placeholder="Pesquise pelo paciente"/>
-            </div>
-        </section>
-    );
+  };
+
+  return (
+    <section className="Homelog-sec">
+      <div className="credencial-log">
+        <div className="text-log">
+          <p>
+            Usuário: <span>{localStorage.getItem("name")}</span>
+          </p>
+          <p>
+            Cargo: <span>{localStorage.getItem("cargo")}</span>
+          </p>
+        </div>
+        <div className="btn-log">
+          <button onClick={logout} className="logout">
+            LogOut
+          </button>
+          <button onClick={deleteAccount} className="delete">
+            Delete
+          </button>
+        </div>
+      </div>
+      <div className="input_pesquisa">
+        <input
+          type="text"
+          placeholder="Pesquise pelo paciente"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+        <button onClick={procuraNoDadosJson}>Pesquisar</button>
+      </div>
+      <div className="resultados-pesquisa">
+        <h2>Resultados da Pesquisa:</h2>
+        <ul>
+          {searchResults.map((paciente) => (
+            <li key={paciente.id}>
+              <p>Nome: {paciente.nome}</p>
+              <p>Idade: {paciente.idade}</p>
+              <p>Exames:</p>
+              <ul>
+                {paciente.exames.map((exame) => (
+                  <li key={exame.id}>
+                    <p>Tipo: {exame.tipo}</p>
+                    <p>Resultado: {exame.resultado}</p>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
 }
+
 export default Homelog;
